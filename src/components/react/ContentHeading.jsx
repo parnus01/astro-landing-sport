403ad15c1d9861331d8c1571dd4b04c20b9d8cbc
Danusorn.t <osacn_danusorn.taboonyong@krungthai.com>
2024-01-21 10:54:04 +0700

    fix(style): update layout mobile overlap / img sizing

--- src/components/react/ContentHeading.jsx
+++ src/components/react/ContentHeading.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 // Define the React component
 const ContentHeading = ({ heading }) => {
-    return (
-        <div className="sm:hidden md:block text-grey lg:mb-[70px] md:mb-[45px] lg:pt-[40px] md:pt-[20px] sm:p-5 md:p-0 md:leading-[105px] sm:leading-[56px] sm:text-[50px] lg:text-[90px] md:text-[90px]">
-            {heading}
-        </div>
-    );
+  return (
+    <div className="text-grey sm:hidden sm:p-5 sm:text-[50px] sm:leading-[56px] md:z-0 md:mb-[45px] md:block md:p-0 md:pt-[20px] md:text-[90px] md:leading-[105px] lg:mb-[70px] lg:pt-[40px] lg:text-[90px]">
+      {heading}
+    </div>
+  );
 };
 
 export default ContentHeading;
